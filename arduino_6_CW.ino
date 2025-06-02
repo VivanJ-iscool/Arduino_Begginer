75e416e0d5143f96deca0ad7116f6f7a2143fa7c
VivanJ-iscool <vivanrocksio@gmail.com>
2025-06-02 19:00:13 -0400

    Update arduino_6_CW.ino

--- arduino_6_CW.ino
+++ arduino_6_CW.ino
@@ -1,27 +1,51 @@
-int buttonPin = 9  //LED pin
-boolean isLighting  //defines a variable to save LED state
+//6.1 Push Button controlled LED
+
+// int buttonPin = 12;  //LED pin
+// int ledPin = 9;
+
+// void setup() {
+//   pinMode(buttonPin, INPUT);
+//   pinMode(ledPin, OUTPUT);
+// }
+
+
+// void loop() {
+//   if (digitalRead(buttonPin) == HIGH)
+//     digitalWrite(ledPin, LOW);
+//   else;
+//     digitalWrite(ledPin, LOW);
+// }
+
+
+//6.2 LED State change using Push Button
+
+int buttonPin = 12;
+int ledPin = 9;
+boolean isLightning = false;
 
 void setup() {
   pinMode(buttonPin, INPUT);
   pinMode(ledPin, OUTPUT);
 }
 
-void loop() {
-  if (digitalRead(buttonPin)) == LOW {  //if button's pressed
+void loop(){
+if (digitalRead(buttonPin) == LOW){
+  delay(10);
+  if (digitalRead(buttonPin) == LOW){
+    reverseLED();
+    while (digitalRead(buttonPin)==LOW);
     delay(10);
-    if (digitalRead(buttonPin)) == LOW {  //double check for button press
-      reverseLED()  //reverse LED
-
-
-
-
-
-
-
-
-
-
-
     }
+  }
+}
+
+void reverseLED() {
+  if (isLightning) {
+    digitalWrite(ledPin, LOW);
+    isLightning=false;
+  }
+  else {
+    digitalWrite(ledPin, HIGH);
+    isLightning = true;
   }
 }
